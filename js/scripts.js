b6f447538216a60137eb1f8957802910585eb012
Tommy Alter <tomealter@gmail.com>
2017-03-22 16:38:57 -0400

    remade with a nicer background

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,6 @@ $(document).ready(function() {
   // init controller
 	var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
 
-	// build scenes
-	// new ScrollMagic.Scene({triggerElement: "#parallax-1"})
-	// 				.setTween("#parallax-1 > .section__image.red", {y: "10%", ease: Linear.easeNone})
-	// 				.addTo(controller);
-
-  // new ScrollMagic.Scene({triggerElement: "#parallax-1"})
-	// 				.setTween("#parallax-1 > .section__image.layer-1", {y: "50%", ease: Linear.easeNone})
-	// 				.addTo(controller);
-
-	// new ScrollMagic.Scene({triggerElement: "#parallax-1"})
-	// 				.setTween("#parallax-1 > .section__content", {y: "800%", ease: Linear.easeNone})
-	// 				.addTo(controller);
-
   new ScrollMagic.Scene({triggerElement: "#parallax-1"})
 					.setTween("#parallax-1 > .section__image.layer-2", {y: "50%", ease: Linear.easeNone})
 					.addTo(controller);
@@ -28,7 +15,11 @@ $(document).ready(function() {
 					.addTo(controller);
 
 	new ScrollMagic.Scene({triggerElement: "#parallax-1"})
-					.setTween("#parallax-1 > .section__image.layer-5", {y: "150%", ease: Linear.easeNone})
+					.setTween("#parallax-1 > .section__image.layer-5", {y: "140%", ease: Linear.easeNone})
+					.addTo(controller);
+
+	new ScrollMagic.Scene({triggerElement: "#parallax-1"})
+					.setTween("#parallax-1 > .section__image.layer-6", {y: "140%", ease: Linear.easeNone})
 					.addTo(controller);
 
 
